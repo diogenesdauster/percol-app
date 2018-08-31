2717d40e40c6b38b8507d2dff84c51489115b34a
disias <diogenes.dauster@gmail.com>
2018-08-30 23:57:36 -0300

    feat: Update the Login View
    add two inputs

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  KeyboardAvoidingView
+} from "react-native";
 import { Hideo } from "react-native-textinput-effects";
 import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
 
@@ -7,20 +12,8 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.inputsgroup}>
-          <Hideo
-            style={styles.inputs}
-            iconClass={FontAwesomeIcon}
-            iconName={"user"}
-            iconColor={"white"}
-          />
-          <Hideo
-            style={styles.inputs}
-            iconClass={FontAwesomeIcon}
-            iconName={"key"}
-            iconColor={"white"}
-          />
-        </View>
+        <TextInput style={styles.inputs} />
+        <TextInput style={styles.inputs} />
       </View>
     );
   }
@@ -34,12 +27,15 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   },
   inputsgroup: {
-    flex: 1,
     justifyContent: "space-between"
   },
   inputs: {
-    flex: 0,
-    marginLeft: 30,
-    marginRight: 30
+    backgroundColor: "white",
+    width: 300,
+    height: 50,
+    borderRadius: 20,
+    shadowRadius: 2,
+    shadowOpacity: 0.8,
+    elevation: 3
   }
 });
